1f0debdd3fd72fab2449d9df1fbdcf7eeaee293a
Wilfred Hughes <me@wilfred.me.uk>
2012-10-13 22:35:06 +0100

    factoring out different blocks as separate parsing functions

--- Creole.hs
+++ Creole.hs
@@ -20,34 +20,43 @@ lineContent =
               rest <- lineContent
               return $ whitespace ++ rest)
   <|> return ""
+      
+heading1 = do
+  string "="
+  spaces
+  heading <- many $ noneOf "\n"
+  newline
+  return $ "<h1>" ++ heading ++ "</h1>"
+heading2 = do
+  string "=="
+  spaces
+  heading <- many $ noneOf "\n"
+  newline
+  return $ "<h2>" ++ heading ++ "</h2>"
+  
+paragraph = do
+  paragraph <- lineContent
+  newline
+  newline
+  return $ "<p>" ++ paragraph ++ "</p>"
+  
+nowiki = do
+  string "{{{"
+  nowiki <- many (try (string "}" >> noneOf "}") <|> try (string "}}" >> noneOf "}") <|> noneOf "}")
+  string "}}}"
+  return $ "<pre>" ++ nowiki ++ "</pre>"
+  
 
 -- either a paragraph or a heading
 -- TODO: headings may end with matching == too
 -- everything ends with a newline, except paragraphs, which require two
 block :: Parser String
 block = 
-  try (do
-    string "=="
-    spaces
-    heading <- many $ noneOf "\n"
-    newline
-    return $ "<h2>" ++ heading ++ "</h2>")
-  <|> try (do
-    char '='
-    spaces
-    heading <- many $ noneOf "\n"
-    newline
-    return $ "<h1>" ++ heading ++ "</h1>")
-  <|> try (do
-    paragraph <- lineContent
-    newline
-    newline
-    return $ "<p>" ++ paragraph ++ "</p>")
-  <|> try (do
-    string "{{{"
-    nowiki <- many (try (string "}" >> noneOf "}") <|> try (string "}}" >> noneOf "}") <|> noneOf "}")
-    string "}}}"
-    return $ "<pre>" ++ nowiki ++ "</pre>")
+  -- need to match subsubheadings first
+      try heading2
+  <|> try heading1
+  <|> try paragraph
+  <|> try nowiki
   
 -- parse Creole 1.0 source and return HTML
 -- requires a trailing newline
